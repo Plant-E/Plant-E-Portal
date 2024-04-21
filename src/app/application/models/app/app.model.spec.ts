96f953b6b620bd073b74d37cf7c5400f5184b009
piotr <piotr@infordb.com>
2024-04-21 14:43:47 +0200

    Angular -> Failed unit test fix

--- src/app/application/models/app/app.model.spec.ts
+++ src/app/application/models/app/app.model.spec.ts
@@ -1,7 +1,13 @@
 import { App } from './app.model';
+import {AppsLibraryService} from "../../services/appsLibrary/apps-library.service";
+import {UtilityService} from "../../services/utility/utility.service";
 
 describe('App', () => {
   it('should create an instance', () => {
-    expect(new App()).toBeTruthy();
+    expect(new App({
+      key: 'poc',
+      name: 'Proof of Concept',
+      active: 1,
+    })).toBeTruthy();
   });
 });
